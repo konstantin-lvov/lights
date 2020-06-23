a2371ee8bdec20c050b0441298c5c1efed9bb790
konstantin-lvov <nattts@yahoo.com>
2020-06-23 18:07:25 +0500

    fix all bugs

--- lights.ino
+++ lights.ino
@@ -20,11 +20,13 @@ const byte LOWER_LIMIT = 0;
 const int MOVEMENT_TRGGER = 500; // trashhold for action
 const byte MOVEMENT_NIGH_LIMIT = 5; // light level if night
 const byte MOVEMENT_BRIGHT_TRIGGER = 20;
-const long TIME_FOR_MOVE = 3e5;
-const int TIME_FOR_ON_OR_OFF = 1000; //millisec
+long TIME_FOR_MOVE = 3e5;
+const int TIME_FOR_ON_OR_OFF = 500; //millisec
+
 const float MIN_BAT_VOLTAGE = 3.5;
 const float MAX_BAT_VOLTAGE = 4.15;
 const float COEF = 1;
+const byte MULTIPLIER = 8;
 
 bool lightIsOn = false;
 bool powerIsOn = false;
@@ -32,8 +34,9 @@ bool isCharging = false;
 bool lightsByMove = false;
 bool onByMovementSensor = false;
 bool nightLightsOn = false;
+bool dedTime = false;
 
-byte current_light_levels[3] = {LOWER_LIMIT, LOWER_LIMIT, LOWER_LIMIT}; //текущие уровни освещения
+byte current_light_levels[] = {LOWER_LIMIT, LOWER_LIMIT, LOWER_LIMIT}; //текущие уровни освещения
 
 byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
 byte mosfets[3] = {TABLE_LIGHTS_MOSFET, KITCHEN_LIGHTS_MOSFET, HALL_LIGHTS_MOSFET}; // номера пинов для управления мосфетами
@@ -41,22 +44,24 @@ byte delay_for_changing_lights = 3;
 byte pipeNo, gotByte, oldMaxLightLevel = 0, calculatedMaxLightLevel;
 
 int photo_resistor = 0;
+int counter = 0;
 
 float battery_voltage = 0;
 
 double maxLightLevel = 0;
 
-long millisOnStart;
+unsigned long millisOnStart;
 unsigned long lastMoveTime;
 unsigned long currentTime;
 
 void setup() {
+
   // Пины D5 и D6 - 7.8 кГц
   TCCR0B = 0b00000010;  // x8
   TCCR0A = 0b00000011;
-  // Пины D5 и D6 - 7.8 кГц
-  TCCR0B = 0b00000010;  // x8
-  TCCR0A = 0b00000011;
+  // Пины D3 и D11 - 8 кГц
+  TCCR2B = 0b00000010;  // x8
+  TCCR2A = 0b00000011;  // fast pwm
 
   //Serial.begin(9600); //открываем порт для связи с ПК
   radio.begin(); //активировать модуль
@@ -97,9 +102,33 @@ void loop() {
 
   if (onByMovementSensor) {
     if (analogRead(movement_sensor) > MOVEMENT_TRGGER) {
+      Serial.print("MOVEMENT SENSOR VALUE: ");
+      Serial.println(movement_sensor);
       lastMoveTime  = millis();
+      while (!dedTime) {
+        dedTime = true;
+        for (int x = 0; x < 50; x++) {
+          if (analogRead(movement_sensor) > MOVEMENT_TRGGER) {
+            dedTime = false;
+          }
+          delay(2);
+        }
+      }
+      dedTime = false;
     }
-    if (currentTime - lastMoveTime > TIME_FOR_MOVE) {
+    Serial.print("lastMoveTime: ");
+    Serial.println(lastMoveTime);
+    Serial.print("currentTime: ");
+    Serial.println(currentTime);
+    Serial.println(lastMoveTime - currentTime);
+    if (nightLightsOn) {
+      TIME_FOR_MOVE = 3e5;
+    } else {
+      TIME_FOR_MOVE = 3e5 * MULTIPLIER;
+    }
+    if (currentTime > lastMoveTime &&
+        currentTime - lastMoveTime > TIME_FOR_MOVE) {
+      Serial.println("DISABLE LIGHTS BECAUSE TIME IS UP");
       lightsOff();
       onByMovementSensor = false;
     }
@@ -110,6 +139,14 @@ void loop() {
     // коррректируем яркость по фоторезистору
     photo_resistor = analogRead(ph_resistor_pin);
     Serial.print("photo resistor: "); Serial.println(photo_resistor);
+
+    maxLightLevel = 0.004 * pow((photo_resistor * COEF), 2); //0 - 150 = 150
+    if (maxLightLevel < LOW_LIGHT) {
+      maxLightLevel = LOW_LIGHT;
+    }
+    if (maxLightLevel > 250) {
+      maxLightLevel = 250;
+    }
 
     // снимаем показания с батареи
     battery_voltage = (float)analogRead(battery_volt_pin) / 1023 * 5.0;
@@ -125,17 +162,18 @@ void loop() {
       }
     }
 
-    if (gotByte > 0 && gotByte < 255) {
+    if (gotByte > 0 && gotByte < 4) {
       Serial.print("Recieved: "); Serial.println(gotByte);
-      applyCommand(gotByte);
+      applyCommand();
       gotByte = 0;
     }
-    delay(100);
+    delay(50);
 
     postAdjustment();
 
     if (!lightIsOn) {
       if (analogRead(movement_sensor) > MOVEMENT_TRGGER) {
+        Serial.println("MOVEMENT");
         movementSensorHendler();
       }
     }
@@ -151,21 +189,13 @@ void loop() {
 
 }
 
-void applyCommand(byte lightningOptions) {
-  if (lightningOptions > 0 && lightningOptions < 4) {
-    maxLightLevel = 0.004 * pow((photo_resistor * COEF), 2); //0 - 150 = 150
-    if (maxLightLevel < LOW_LIGHT) {
-      maxLightLevel = LOW_LIGHT;
-    }
-    if (maxLightLevel > 250) {
-      maxLightLevel = 250;
-    }
-    if (lightIsOn) {
-      lightsOff();
-    } else {
-      lightsOn();
-    }
+void applyCommand() {
+  if (lightIsOn) {
+    lightsOff();
+  } else {
+    lightsOn();
   }
+
 }
 
 void lightsOn() {
@@ -174,28 +204,43 @@ void lightsOn() {
   delay_for_changing_lights = TIME_FOR_ON_OR_OFF / maxLightLevel;
   Serial.print("delay_for_changing_lights: ");
   Serial.println(delay_for_changing_lights);
+  calculatedMaxLightLevel = maxLightLevel;
   digitalWrite(rele_pin, LOW);
   /*
-     Пока свет не включится, включаем на всех потребителях
+     Пока свет не включится до максимального уровня, включаем на всех потребителях
   */
+  Serial.println("before while");
   while (!lightIsOn) {
+    Serial.println("in begin while");
+    Serial.print("calculatedMaxLightLevel: "); Serial.println(calculatedMaxLightLevel);
     for (byte x = 0; x < sizeof(mosfets); x++) {
-      if (current_light_levels[x] < maxLightLevel) {
+
+      if (current_light_levels[x] < calculatedMaxLightLevel) {
         current_light_levels[x] += STEP;
         analogWrite(mosfets[x], current_light_levels[x]);
+        Serial.print(x); Serial.print(" current_light_level -"); Serial.println(current_light_levels[x]);
       }
-      /*
-               Если уровень яркости всех потребителей на максимуме значит свет включен
-      */
-      if (current_light_levels[TABLE_LIGHTS_MOSFET] == maxLightLevel &&
-          current_light_levels[KITCHEN_LIGHTS_MOSFET] == maxLightLevel &&
-          current_light_levels[HALL_LIGHTS_MOSFET] == maxLightLevel) {
-        lightIsOn = true;
-      }
-      delay(delay_for_changing_lights);
     }
+    /*
+             Если уровень яркости всех потребителей на максимуме значит свет включен
+    */
+    for (byte x = 0; x < sizeof(mosfets); x++) {
+      counter += current_light_levels[x];
+    }
+    Serial.print("counter: "); Serial.println(counter);
+    if (counter == calculatedMaxLightLevel * sizeof(mosfets)) {
+      Serial.println("Condition true - all lights on");
+      lightIsOn = true;
+      counter = 0;
+      break;
+    }
+    counter = 0;
+    delay(delay_for_changing_lights);
+
+    Serial.println("in the end of while");
   }
-  oldMaxLightLevel - maxLightLevel;
+  Serial.println("after while");
+  oldMaxLightLevel = calculatedMaxLightLevel;
 }
 
 void lightsOff() {
@@ -204,6 +249,7 @@ void lightsOff() {
   delay_for_changing_lights = TIME_FOR_ON_OR_OFF / maxLightLevel;
   Serial.print("delay_for_changing_lights: ");
   Serial.println(delay_for_changing_lights);
+  calculatedMaxLightLevel = maxLightLevel;
   /*
      Пока свет не выключится, гасим его на всех потребителях
   */
@@ -212,17 +258,25 @@ void lightsOff() {
       if (current_light_levels[x] > LOWER_LIMIT) {
         current_light_levels[x] -= STEP;
         analogWrite(mosfets[x], current_light_levels[x]);
+        Serial.print(x); Serial.print(" current_light_level -"); Serial.println(current_light_levels[x]);
       }
-      /*
-         Если уровень яркости всех потребителей на минимальном значении значит свет выключен
-      */
-      if (current_light_levels[TABLE_LIGHTS_MOSFET] == LOWER_LIMIT &&
-          current_light_levels[KITCHEN_LIGHTS_MOSFET] == LOWER_LIMIT &&
-          current_light_levels[HALL_LIGHTS_MOSFET] == LOWER_LIMIT) {
-        lightIsOn = false;
-      }
-      delay(delay_for_changing_lights);
     }
+    /*
+       Если уровень яркости всех потребителей на минимальном значении значит свет выключен
+    */
+    for (byte x = 0; x < sizeof(mosfets); x++) {
+      counter += current_light_levels[x];
+    }
+    Serial.print("counter: "); Serial.println(counter);
+    if (counter == LOWER_LIMIT) {
+      Serial.println("Condition true - all lights off");
+      lightIsOn = false;
+      counter = 0;
+      break;
+    }
+    counter = 0;
+    delay(delay_for_changing_lights);
+
   }
   /*
      Если батарея сейчас не заряжается то можно выключить
@@ -230,11 +284,12 @@ void lightsOff() {
   if (!isCharging) {
     digitalWrite(rele_pin, HIGH);
   }
-  delay(1000);
+  
+  onByMovementSensor = false;
 }
 
 void postAdjustment() {
-  if (lightIsOn) {
+  if (lightIsOn && !nightLightsOn) {
 
     maxLightLevel = 0.004 * pow((photo_resistor * COEF), 2);
     if (maxLightLevel < LOW_LIGHT) {
@@ -281,20 +336,33 @@ void postAdjustment() {
 
 void movementSensorHendler () {
   if (photo_resistor < MOVEMENT_BRIGHT_TRIGGER) {
+    Serial.print("HALL LIGHT ON!");
     analogWrite(TABLE_LIGHTS_MOSFET, 0);
     analogWrite(KITCHEN_LIGHTS_MOSFET, 0);
     analogWrite(HALL_LIGHTS_MOSFET, 0);
     delay_for_changing_lights = TIME_FOR_ON_OR_OFF / MOVEMENT_NIGH_LIMIT;
     digitalWrite(rele_pin, LOW);
-    while (current_light_levels[HALL_LIGHTS_MOSFET] < MOVEMENT_NIGH_LIMIT) {
-      current_light_levels[HALL_LIGHTS_MOSFET] += STEP;
-      analogWrite(mosfets[HALL_LIGHTS_MOSFET], current_light_levels[HALL_LIGHTS_MOSFET]);
+    while (current_light_levels[2] < MOVEMENT_NIGH_LIMIT) {
+      current_light_levels[2] += STEP;
+      analogWrite(HALL_LIGHTS_MOSFET, current_light_levels[2]);
       delay(delay_for_changing_lights);
     }
     lightIsOn = true;
     nightLightsOn = true;
   } else {
+    Serial.println("ALL LIGHTS ON BY MOVEMENT");
     lightsOn();
   }
+  lastMoveTime = millis();
   onByMovementSensor = true;
+  while (!dedTime) {
+    dedTime = true;
+    for (int x = 0; x < 50; x++) {
+      if (analogRead(movement_sensor) > MOVEMENT_TRGGER) {
+        dedTime = false;
+      }
+      delay(2);
+    }
+  }
+  dedTime = false;
 }
